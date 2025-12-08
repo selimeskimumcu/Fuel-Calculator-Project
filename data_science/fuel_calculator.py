ff64c4286da5d3bd542a464e4f91bd3cd6931ea7
Selim Eskimumcu <selimeskimumcu@gmail.com>
2025-12-08 23:02:29 +0300

    Add fuel calculator with city-highway support

--- data_science/fuel_calculator.py
+++ data_science/fuel_calculator.py
@@ -12,4 +12,41 @@ def calculate_cost(fuel_usage_litre, fuel_price_per_litre):
     return fuel_usage_litre * fuel_price_per_litre
 
     
+def calculate_mixed_consumption(city_ratio, highway_ratio, city_consumption, highway_consumption):
+    """
+    Parameters
+    city_ratio: The route's urban ratio (between 0 and 1)
+    highway_ratio: The route's extra-urban ratio (between 0 and 1)
+    city_consumption: The vehicle's urban consumption (L/100km)
+    highway_consumption: The vehicle's extra-urban consumption (L/100km)
+
+    """
+    total = city_ratio + highway_ratio
+    if total == 0:
+        raise ValueError("city_ratio + highway_ratio cant be 0.")
+            
+    city_ratio_norm = city_ratio / total
+    highway_ratio_norm = highway_ratio / total
+    
+    mixed = (city_ratio * city_consumption) + (highway_ratio * highway_consumption)
+    return mixed   # L/100km
+
+def calculate_trip_cost_with_mixed_consumption(distance_km,city_ratio,highway_ratio,city_consumption,highway_consumption,fuel_price):
+    mixed_consumption = calculate_mixed_consumption(
+        city_ratio, highway_ratio, city_consumption, highway_consumption)
+    
+    fuel_used = calculate_fuel_usage(distance_km, mixed_consumption)
+    total_cost = calculate_cost(fuel_used, fuel_price)
+    
+    return {
+        "mixed_consumption": mixed_consumption,
+        "fuel_used": fuel_used,
+        "total_cost":total_cost
+        }
+
+
+
+
+    
+        
     
--- main.py
+++ main.py
@@ -4,16 +4,33 @@ Created on Mon Dec  8 21:09:54 2025
 
 @author: selim
 """
-from data_science.fuel_calculator import calculate_fuel_usage, calculate_cost
+from data_science.fuel_calculator import calculate_trip_cost_with_mixed_consumption
+
+# Example of Map API Gives that distance
+distance_km = 20.5
+
+# Sample Rates
+city_ratio = 0.6
+highway_ratio = 0.4
+
+# Consumption values ​​of the vehicle (L/100km)
+city_consumption = 8.0
+highway_consumption = 5.5
+
+# Fuel Price (TL/L)
+fuel_price = 43
+
+result = calculate_trip_cost_with_mixed_consumption(
+    distance_km=distance_km,
+    city_ratio=city_ratio,
+    highway_ratio=highway_ratio,
+    city_consumption=city_consumption,
+    highway_consumption=highway_consumption,
+    fuel_price=fuel_price
+)
+
+print("Karma tüketim (L/100km):", result["mixed_consumption"])
+print("Toplam yakıt (L):", result["fuel_used"])
+print("Toplam maliyet (TL):", result["total_cost"])
 
 
-distance=150 #km
-avg_consumption= 6.5 # L/100km
-fuel_price = 50 # TL/Litres
-
-
-fuel = calculate_fuel_usage(distance, avg_consumption)
-total_cost = calculate_cost(fuel, fuel_price)
-
-print(f"Average fuel consumption: {fuel} L")
-print(f"Cost: {total_cost} TL")
