3e1617c07a7d0562c95bec68cb8bab2e5d65da51
Selim Eskimumcu <selimeskimumcu@gmail.com>
2025-12-08 21:00:36 +0300

    data science updated.

---
+++ data_science/fuel_calculator.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  8 20:58:33 2025
+
+@author: selim
+"""
+
---
+++ data_science/route_analysis.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  8 20:59:20 2025
+
+@author: selim
+"""
+
