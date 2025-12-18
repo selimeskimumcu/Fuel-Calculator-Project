7ffc3f097e496e74edaee6c87fedf5b7b39fad28
Selim Eskimumcu <selimeskimumcu@gmail.com>
2025-12-18 18:33:57 +0300

    Yakıt API eklendi ve güncellendi.

--- data_science/route_analysis.py
+++ data_science/route_analysis.py
@@ -4,4 +4,31 @@ Created on Mon Dec  8 20:59:20 2025
 
 @author: selim
 """
+from data_science.vehicle_data import find_vehicle_consumption
+from data_science.fuel_calculator import calculate_trip_cost_with_mixed_consumption
+from data_science.fuel_prices import get_fuel_price_istanbul
 
+def estimate_trip_istanbul_with_live_fuel_price(
+    distance_km,
+    make,
+    model,
+    year,
+    fuel_type="benzin",        # "benzin" | "mazot" | "lpg"
+    region="AVRUPA",           # "AVRUPA" | "ANADOLU"
+    city_ratio=0.6,
+    highway_ratio=0.4
+):
+    city_c, highway_c = find_vehicle_consumption(make, model, year)
+    if city_c is None or highway_c is None:
+        raise ValueError("Seçilen araç için yakıt tüketim verisi bulunamadı.")
+
+    fuel_price = get_fuel_price_istanbul(fuel_type=fuel_type, region=region)
+
+    return calculate_trip_cost_with_mixed_consumption(
+        distance_km=distance_km,
+        city_ratio=city_ratio,
+        highway_ratio=highway_ratio,
+        city_consumption=city_c,
+        highway_consumption=highway_c,
+        fuel_price=fuel_price
+    )
--- main.py
+++ main.py
@@ -4,33 +4,22 @@ Created on Mon Dec  8 21:09:54 2025
 
 @author: selim
 """
-from data_science.fuel_calculator import calculate_trip_cost_with_mixed_consumption
+from data_science.route_analysis import estimate_trip_istanbul_with_live_fuel_price
 
-# Example of Map API Gives that distance
-distance_km = 20.5
-
-# Sample Rates
-city_ratio = 0.6
-highway_ratio = 0.4
-
-# Consumption values ​​of the vehicle (L/100km)
-city_consumption = 8.0
-highway_consumption = 5.5
-
-# Fuel Price (TL/L)
-fuel_price = 43
-
-result = calculate_trip_cost_with_mixed_consumption(
-    distance_km=distance_km,
-    city_ratio=city_ratio,
-    highway_ratio=highway_ratio,
-    city_consumption=city_consumption,
-    highway_consumption=highway_consumption,
-    fuel_price=fuel_price
+result = estimate_trip_istanbul_with_live_fuel_price(
+    distance_km=25.4,
+    make="ACURA",
+    model="1.6EL",
+    year=2000,
+    fuel_type="benzin",
+    region="AVRUPA",
+    city_ratio=0.7,
+    highway_ratio=0.3
 )
 
-print("Karma tüketim (L/100km):", result["mixed_consumption"])
-print("Toplam yakıt (L):", result["fuel_used"])
-print("Toplam maliyet (TL):", result["total_cost"])
+print(result)
+
+
+
 
 
